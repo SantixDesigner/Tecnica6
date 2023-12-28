1cfcee9982a5ed4a4642eef7229e0917ccb42983
Santi <xxsmating@gmail.com>
2023-12-28 09:50:08 -0300

    ...

--- src/components/Preinscripcion/Main.tsx
+++ src/components/Preinscripcion/Main.tsx
@@ -35,7 +35,7 @@ const Preinscripcion = () => {
         }
 
         try {
-            const response = await fetch('https://backprueba-d72n3pyjc-santixdesigner.vercel.app/form', {
+            const response = await fetch('https://backprueba-qc26x0amm-santixdesigner.vercel.app/form', {
                 method: 'POST',
                 body: formData,  // No necesitas JSON.stringify() aquí
             });
