f59e9b11b234ded274dddce196e55f87825228b1
Santi <xxsmating@gmail.com>
2023-12-28 10:06:16 -0300

    ...

--- src/components/Preinscripcion/Main.tsx
+++ src/components/Preinscripcion/Main.tsx
@@ -36,7 +36,7 @@ const Preinscripcion = () => {
 
         try {
             const response = await fetch('https://backprueba-nbigo6xys-santixdesigner.vercel.app/form', {
-                "mode": "no-cors",
+                mode: "no-cors",
                 method: 'POST',
                 body: formData,  // No necesitas JSON.stringify() aquí
             });
